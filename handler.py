aa975cc04d1e4060683ab64a9e2642f6e38e9931
Colton Myers <colton.myers@gmail.com>
2021-10-19 11:37:10 -0600

    Bunch of updates

--- handler.py
+++ handler.py
@@ -1,21 +1,37 @@
 import json
+import time
+
+import requests
+from elasticapm import capture_serverless, capture_span
 
 
+@capture_serverless()
+def hello2(event, context):
+    r = requests.get("https://elastic.co")
+    return {"statusCode": r.status_code, "body": "Success!"}
+
+
+@capture_serverless()
 def hello(event, context):
+    time.sleep(0.15)
+
+    _do_work()
+
     body = {
-        "message": "Go Serverless v2.0! Your function executed successfully!",
-        "input": event,
+        "message": "Hello world!",
+        "event": event,
+        "context": str(vars(context)),
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
+    print(response)
+
+    print(json.dumps(event))
+
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+
+@capture_span()
+def _do_work():
+    time.sleep(0.35)
